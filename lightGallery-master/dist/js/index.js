cabed03425dc9a73d8d48ca15a74ca0d8f367320
Abeer-Eva <54839100+Abeer-Eva@users.noreply.github.com>
2020-12-20 18:28:19 +0100

    lightbox

--- lightGallery-master/dist/js/index.js
+++ lightGallery-master/dist/js/index.js
@@ -6,15 +6,14 @@ const searchTerm = document.querySelector('#query');
 
 
 
-
-
 //Form function för att skriva och söka ord
 form.addEventListener('submit', e => {
-    e.preventDefault();
-    console.log(searchTerm.value);      //consolar värdet av det som skrivs in i input
-    getData(searchTerm.value);
+  e.preventDefault();
+  console.log(searchTerm.value);      //consolar värdet av det som skrivs in i input
+  getData(searchTerm.value);
 });
-//Fetch function som hämtar data från Flickr API
+
+//Async function with searchword.value as a parameter that gets the images from the url 
 async function getData(query) {
   const response = await fetch(`https://www.flickr.com/services/rest/?api_key=${key}&method=flickr.photos.search&text=${query}&per_page=20&extras=media&content_type=1&
   &sort=relevance&format=json&nojsoncallback=1`);
@@ -26,67 +25,104 @@ async function getData(query) {
 function showPhotos(array) {
   console.log(array)
   //rensar listan inför varje sökning
-  lightGallery.innerHTML ="";      
+  lightGallery.innerHTML = "";
   //itererar genom array av json.data.photos.photo[0],[1],[2] osv...
   array.forEach(value => {
-    listUrl= `https://farm4.staticflickr.com/${value.server}/${value.id}_${value.secret}_m.jpg`;
+    listUrl = `https://farm4.staticflickr.com/${value.server}/${value.id}_${value.secret}_m.jpg`;
     //Sparar och consolar Url som skall generera bilder på webbappen.skrivs över med value värden från loopen
-    console.log(listUrl); 
-     //skapar ny list element
+    console.log(listUrl);
+
+    //skapar ny list element
     const item = document.createElement('li');
     //skapar classer på listelementen
-    item.classList.add('gallery'); 
+    item.classList.add("gallery");
+    
     //skapar img taggar
-    item.innerHTML = `<img src="${listUrl}" alt="${value.title}"  id="ID" herf=""></img>`; 
+    item.innerHTML = `<a href="https://live.staticflickr.com/${value.server}/${value.id}_${value.secret}_b.jpg">`+`<img src="https://live.staticflickr.com/${value.server}/${value.id}_${value.secret}_b.jpg" class="img-responsive">`+`</a>`;
+    
+ 
+ 
+
+    
     //lägger till img items på listan
-    lightGallery.appendChild(item); 
-    console.log(value.title);
+    lightGallery.appendChild(item);
+    console.log(value.title, value.id);
 
   });
-};
 
 
+  $('#lightGallery').lightGallery();
 
 
-//click på en bild som skicka sen till lightbox
-document.getElementById('lightGallery').onclick= ()=>
-{
-
-//  Lightbox 
-//thumb
-$('#aniimated-thumbnials').lightGallery({
-  thumbnail:true
-}); 
-//responsive bilder
-$('#responsive-images').lightGallery(); 
-var $methods = $('#methods');
-var slide = '<a href="img/img2.jpg">' +
-    '<img src="img/thumb2.jpg" />' +
-'</a>';
- 
-$methods.lightGallery();
-$('#appendSlide').on('click', function() {
-    $methods.append(slide);
-    $methods.data('lightGallery').destroy(true);
-    $methods.lightGallery();
-});
-$('#fixed-size').lightGallery({
-  width: '700px',
-  height: '470px',
-  mode: 'lg-fade',
-  addClass: 'fixed-size',
-  counter: false,
-  download: false,
-  startClass: '',
-  enableSwipe: false,
-  enableDrag: false,
-  speed: 500
-});
-
-
-$('#custom-transitions').lightGallery({
-  mode: 'lg-fade'
-})
- 
-
-}
+  //  Lightbox 
+  $(document).ready(function() {
+    
+      $("#lightGallery").lightGallery();
+    
+    });
+    $("#lightGallery").lightGallery({
+      
+        mode:'lg-slide',
+  
+       
+        // Ex : 'ease'
+    
+        cssEasing:'ease',
+    
+        //'for jquery animation'
+      
+        easing:'linear',
+      
+        speed: 600,
+  
+        height:'100%',
+        width:'100%',
+        addClass:'',
+      
+        startClass:'lg-start-zoom',
+        backdropDuration: 150,
+        // Set 0, if u don't want to hide the controls
+        hideBarsDelay: 6000,
+        useLeft:false,
+        // aria-labelledby attribute fot gallery
+  
+        ariaLabelledby: '',
+        //aria-describedby attribute for gallery
+    
+        ariaDescribedby: '',
+        closable: true,
+        loop: true,
+        escKey: true,
+        keyPress: true,
+        controls: true,
+        slideEndAnimatoin: true,
+        hideControlOnEnd: false,
+        mousewheel: true,
+        getCaptionFromTitleOrAlt: true,
+        // .lg-item || '.lg-sub-html'
+        appendSubHtmlTo: '.lg-sub-html',
+        subHtmlSelectorRelative: false,
+        preload: 1,
+        showAfterLoad: true,
+        selector: '',
+        selectWithin: '',
+      
+        nextHtml: '',
+        prevHtml: '',
+        // 0, 1
+        index: false,
+        iframeMaxWidth: '100%',
+        download: true,
+        counter: true,
+        appendCounterTo: '.lg-toolbar',
+        swipeThreshold: 50,
+        enableSwipe:true,
+        enableDrag:true,
+        dynamic:false,
+        dynamicEl: [],
+    
+        galleryId: 1,
+        supportLegacyBrowser:true
+      
+      });}                     
+          
